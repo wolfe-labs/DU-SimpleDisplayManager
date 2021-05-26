a23405fc7051c4a07a7de84b208f1b431b02d13d
Matheus Pratta <matpratta@gmail.com>
2021-05-26 00:03:36 -0300

    Added new drawing capabilities to match new Render Scripts

--- src/DriverSVG.lua
+++ src/DriverSVG.lua
@@ -11,6 +11,9 @@ local function make(classDef)
   -- Creates instance
   local self = setmetatable({}, metatable)
 
+  -- Optionally initializes it
+  if self.init then self:init() end
+
   -- Returns instance
   return self
 end
@@ -37,15 +40,16 @@ local Context = {
   layers = {},
 
   -- Current context
-  current = nil
+  current = nil,
+
+  -- Current font
+  currentFont = {
+    name = 'Play',
+    size = 14,
+  },
 }
 
-local Layer = {
-  contents = '',
-  strokeWidth = 1,
-  strokeColor = makeColor(1, 1, 1),
-  fillColor = makeColor(0, 0, 0),
-}
+local Layer = {}
 
 -- Creates a new Context object
 function Context.create()
@@ -64,7 +68,15 @@ function Context:render()
 
   -- Processes each of the layers
   for _, layer in pairs(self.layers) do
-    output = output .. layer.contents
+    output = output .. '<g style="position:relative;">'
+    output = output .. layer.contents.image
+    output = output .. layer.contents.rect
+    output = output .. layer.contents.circle
+    output = output .. layer.contents.line
+    output = output .. layer.contents.triangle
+    output = output .. layer.contents.quad
+    output = output .. layer.contents.text
+    output = output .. '</g>'
   end
 
   -- Closes SVG and returns
@@ -87,6 +99,53 @@ function Context:createLayer()
 
   -- Returns instance of the said layer
   return layer
+end
+
+-- Loads a font for usage
+function Context:loadFont(font, size)
+  -- Creates the new font, since we're on SVG all we need is name and size anyways
+  return { name = font, size = size }
+end
+
+-- Loads an image for usage
+function Context:loadImage(path)
+  -- Creates the new image, since we're on SVG all we need is the path
+  return { path = path }
+end
+
+-- Initializes the layer
+function Layer:init()
+  self.contents = {
+    image = '',
+    rect = '',
+    circle = '',
+    line = '',
+    triangle = '',
+    quad = '',
+    text = '',
+  }
+  self.strokeWidth = 1
+  self.strokeColor = makeColor(1, 1, 1)
+  self.fillColor = makeColor(0, 0, 0)
+  self.currentFont = nil
+end
+
+-- Gets active font from layer
+function Layer:getCurrentFont()
+  if self.currentFont then
+    return self.currentFont
+  else
+    return Context.currentFont
+  end
+end
+
+-- Sets active font for layer
+function Layer:setFont(font)
+  -- Updates active font for entire layer
+  self.currentFont = font
+
+  -- Returns self instance for chaining
+  return self
 end
 
 -- Sets current color for fill
@@ -135,7 +194,93 @@ function Layer:drawRect(x, y, width, height)
   local f = self.fillColor
 
   -- "Draws"
-  self.contents = self.contents .. '<rect x="' .. x .. '" y="' .. y .. '" width="' .. width .. '" height="' .. height .. '" style="fill:rgba(' .. f.r .. ', ' .. f.g .. ', ' .. f.b .. ', ' .. f.a .. ');stroke:rgba(' .. s.r .. ', ' .. s.g .. ', ' .. s.b .. ', ' .. s.a .. ');stroke-width:' .. self.strokeWidth .. ';" />'
+  self.contents.rect = self.contents.rect .. '<rect x="' .. x .. '" y="' .. y .. '" width="' .. width .. '" height="' .. height .. '" style="fill:rgba(' .. f.r .. ', ' .. f.g .. ', ' .. f.b .. ', ' .. f.a .. ');stroke:rgba(' .. s.r .. ', ' .. s.g .. ', ' .. s.b .. ', ' .. s.a .. ');stroke-width:' .. self.strokeWidth .. ';" />'
+
+  -- Returns self instance for chaining
+  return self
+end
+
+-- Adds a circle to a layer
+function Layer:drawCircle(centerX, centerY, radius)
+  -- Load stroke color
+  local s = self.strokeColor
+
+  -- Load fill color
+  local f = self.fillColor
+
+  -- "Draws"
+  self.contents.circle = self.contents.circle .. '<circle cx="' .. centerX .. '" cy="' .. centerY .. '" r="' .. radius .. '" style="fill:rgba(' .. f.r .. ', ' .. f.g .. ', ' .. f.b .. ', ' .. f.a .. ');stroke:rgba(' .. s.r .. ', ' .. s.g .. ', ' .. s.b .. ', ' .. s.a .. ');stroke-width:' .. self.strokeWidth .. ';" />'
+
+  -- Returns self instance for chaining
+  return self
+end
+
+-- Adds a line to a layer
+function Layer:drawLine(x1, y1, x2, y2)
+  -- Load stroke color
+  local s = self.strokeColor
+
+  -- Load fill color
+  local f = self.fillColor
+
+  -- "Draws"
+  self.contents.line = self.contents.line .. '<line x1="' .. x1 .. '" y1="' .. y1 .. '" x2="' .. x2 .. '" y2="' .. y2 .. '" style="fill:rgba(' .. f.r .. ', ' .. f.g .. ', ' .. f.b .. ', ' .. f.a .. ');stroke:rgba(' .. s.r .. ', ' .. s.g .. ', ' .. s.b .. ', ' .. s.a .. ');stroke-width:' .. self.strokeWidth .. ';" />'
+
+  -- Returns self instance for chaining
+  return self
+end
+
+-- Adds a quadrilateral to a layer
+function Layer:drawQuad(x1, y1, x2, y2, x3, y3, x4, y4)
+  -- Load stroke color
+  local s = self.strokeColor
+
+  -- Load fill color
+  local f = self.fillColor
+
+  -- "Draws"
+  self.contents.quad = self.contents.quad .. '<path d="M' .. x1 .. ' ' .. y1 .. ' L' .. x2 .. ' ' .. y2 .. ' L' .. x3 .. ' ' .. y3 .. ' L' .. x4 .. ' ' .. y4 .. ' Z" class="quad" style="fill:rgba(' .. f.r .. ', ' .. f.g .. ', ' .. f.b .. ', ' .. f.a .. ');stroke:rgba(' .. s.r .. ', ' .. s.g .. ', ' .. s.b .. ', ' .. s.a .. ');stroke-width:' .. self.strokeWidth .. ';" />'
+
+  -- Returns self instance for chaining
+  return self
+end
+
+-- Adds a triangle to a layer
+function Layer:drawTriangle(x1, y1, x2, y2, x3, y3)
+  -- Load stroke color
+  local s = self.strokeColor
+
+  -- Load fill color
+  local f = self.fillColor
+
+  -- "Draws"
+  self.contents.triangle = self.contents.triangle .. '<path d="M' .. x1 .. ' ' .. y1 .. ' L' .. x2 .. ' ' .. y2 .. ' L' .. x3 .. ' ' .. y3 .. ' Z" class="tri" style="fill:rgba(' .. f.r .. ', ' .. f.g .. ', ' .. f.b .. ', ' .. f.a .. ');stroke:rgba(' .. s.r .. ', ' .. s.g .. ', ' .. s.b .. ', ' .. s.a .. ');stroke-width:' .. self.strokeWidth .. ';" />'
+
+  -- Returns self instance for chaining
+  return self
+end
+
+-- Adds a text to a layer
+function Layer:drawText(text, x, y, font)
+  -- Load fill color
+  local f = self.fillColor
+
+  -- Load active font from the Layer or Context
+  if not font then
+    font = self:getCurrentFont()
+  end
+
+  -- "Draws"
+  self.contents.text = self.contents.text .. '<text x="' .. x .. '" y="' .. y .. '" style="font-size:' .. font.size .. 'px;font-family:\'' .. font.name .. '\';fill:rgba(' .. f.r .. ', ' .. f.g .. ', ' .. f.b .. ', ' .. f.a .. ');">' .. text .. '</text>'
+
+  -- Returns self instance for chaining
+  return self
+end
+
+-- Adds an image
+function Layer:drawImage(image, x, y, width, height)
+    -- "Draws"
+  self.contents.image = self.contents.image .. '<image x="' .. x .. '" y="' .. y .. '" width="' .. width .. '" height="' .. height .. '" href="' .. image.path .. '" />'
 
   -- Returns self instance for chaining
   return self
